08bcc6246fdd7b039575f19deeb82a8e93f48c92
mikeSGman <michael.mcconachie@hotmail.com>
2023-03-18 17:53:38 +0000

    VARIOUS IMPROVEMENTS: - commented out extra test help messages - changed command_prefix from / to ! - added intents bits (for testing later) - changed to yt_dlp library   - bug in youtube_dl was affecting     search results - adjusted search_yt function   - had to change ydl.extract_info     return filter to accommodate yt_dlp     as it uses a slightly different filter - added self.FFMPEG_OPTIONS   - reconnect_at_eof   - reconnect_on_network_error   - multiple_requests - adjusted various functions to use double   quotes for standardization purposes   - search_yt   - play_next - Adjusted comments in commands.command cog   to show the user a more useful message in   discord - fixed indentation issues in command.command cog's   'pause' and 'is_paused' functions to that   precluded the program from actually running - changed queue depth from '5' to '50' in the   queue function - updated requirements.txt

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import discord
 from discord.ext import commands
 import os
 
-#import all of the cogs
+# import all of the cogs
 from help_cog import help_cog
 from music_cog import music_cog
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix="!")
 
-#remove the default help command so that we can write out own
-bot.remove_command('help')
+# remove the default help command so that we can write out own
+bot.remove_command("help")
 
-#register the class with the bot
+# register the class with the bot
 bot.add_cog(help_cog(bot))
 bot.add_cog(music_cog(bot))
 
-#start the bot with our token
+# intents = discord.Intents.default()
+# intents.message_content = True
+# client = MyClient(intents=intents)
+
+# start the bot with our token
 bot.run(os.getenv("TOKEN"))
